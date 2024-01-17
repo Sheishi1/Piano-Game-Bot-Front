0ded65881003b1a9b93e95ca571b66f1f11f571b
Sheishi1 <grikerbek1@mail.ru>
2024-01-17 16:06:17 +0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, {useEffect} from 'react';
+import './App.css';
+// @ts-ignore
+const tg = window.Telegram.WebApp
+
+function App() {
+    useEffect(() => {
+        tg.ready()
+    }, []);
+
+    const onClose = () => {
+        tg.close()
+    }
+
+  return (
+    <div className="App">
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/playground/PlayGroundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayGroundPage = () => {
+    return (
+        <div className="main__playgroundBlock">
+
+        </div>
+    );
+};
+
+export default PlayGroundPage;
