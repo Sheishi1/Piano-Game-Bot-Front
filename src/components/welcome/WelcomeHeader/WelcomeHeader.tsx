c4a2219ccb2d88313b572175a0b72295f95fe000
Sheishi1 <grikerbek1@mail.ru>
2024-01-20 03:16:26 +0500

    test

--- src/components/welcome/WelcomeHeader/WelcomeHeader.tsx
+++ src/components/welcome/WelcomeHeader/WelcomeHeader.tsx
@@ -4,10 +4,10 @@ import "./WelcomeHeader.css"
 const WelcomeHeader = (props: { userName: string, coins: string, points: string}) => {
     return (
         <div className={`main_welcomeHeaderBlock`}>
-            <div className="userName">{props.userName}</div>
+            <div className="userName">{props.userName ? "zxc" : "zxc"}</div>
             <div className="statisticWelcomeBlock">
-                <div className="coinsCount">{props.coins}</div>
-                <div className="maxPoints">{props.points}</div>
+                <div className="coinsCount">{props.coins ? "zxc" : "zxc"}</div>
+                <div className="maxPoints">{props.points ? "zxc" : "zxc"}</div>
             </div>
         </div>
     );
