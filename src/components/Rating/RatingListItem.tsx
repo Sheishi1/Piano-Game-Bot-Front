720b7a49e8cb08719df34e67bf257de89de6d140
Sheishi1 <grikerbek1@mail.ru>
2024-01-23 17:10:27 +0500

    add a rating page

--- src/components/Rating/RatingListItem.tsx
+++ src/components/Rating/RatingListItem.tsx
@@ -5,12 +5,13 @@ import cupSvgIcon from '../../assets/vectorImages/cup-1-svgrepo-com (1).svg'
 
 
 const RatingListItem = (props: {userName: string, points: number, position: number, highlight: boolean}) => {
-    const listItemStyle = props.highlight ? {border: '1px solid white'} : {};
+    const listItemStyle = props.highlight ? {border: '1px solid white', borderRadius: '15px'} : {};
 
     return (
         <div style={listItemStyle}>
             <div className={`main__ratingListItemBlock`}>
-                <span>props.highlight ? {`${props.position}. Вы`} : {`${props.position}. ${props.userName}`}</span>
+
+                <span>{props.highlight ?  `${props.position}. Вы` : `${props.position}. ${props.userName}`}</span>
                 <div className="ratingPointsCount"><img src={cupSvgIcon} alt="replay" style={{marginRight: '5px'}}/>{props.points}</div>
             </div>
         </div>
